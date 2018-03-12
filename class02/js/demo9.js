8d18898e8046977eb6e54f7e0cb63f2b8f7b425f
xunyicaozhilian <1774407914@qq.com>
2018-03-12 19:53:32 +0800

    demo9

---
+++ class02/js/demo9.js
@@ -0,0 +1,18 @@
+var obj = {x:1,y:2};
+var arr = [1,2,3,4,5];
+function foo() {
+    console.log("foo function!");
+};
+console.log(obj instanceof Object);//true
+console.log(arr instanceof Object);//true
+console.log(foo instanceof Object);//true
+console.log(foo === window.foo);//true
+
+
+for(var k in obj){
+    console.log(k,obj[k]);
+}
+
+console.log(Object.keys(obj));
+
+console.log("x" in obj);
