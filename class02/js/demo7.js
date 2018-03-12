7a050c82b5ab93493d0f1bac6eee11eb416f77d5
xunyicaozhilian <1774407914@qq.com>
2018-03-12 19:25:07 +0800

    demo7

---
+++ class02/js/demo7.js
@@ -0,0 +1,11 @@
+//关于字符串的操作
+var str='abc_def_ghi_jkl_mn';
+console.log(str.split('_'));//["abc","def","ghi","jkl","mn"]
+console.log(str.split('_',2));// ["abc", "def"]
+str.concat("_opq");//"abc_def_ghi_jkl_mn_opq"
+str.substr(4,7);//"def_ghi"
+str.substring(4,7);//def
+str.slice(2);//"c_def_ghi_jkl_mn"
+str.slice(2,5);//"c_d"
+str.slice(-2);//"mn"
+str.slice(2,-2);//"c_def_ghi_jkl_"
