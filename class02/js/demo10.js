d293779254d11392091b91818b869e3b0afa58f9
xunyicaozhilian <1774407914@qq.com>
2018-03-14 19:21:08 +0800

    demo10改进

--- class02/js/demo10.js
+++ class02/js/demo10.js
@@ -1,8 +1,13 @@
+//包装对象
+//在基本类型中，如number，string， boolean，其实都有对应的对象类型，但是这不是真正的对象，临时对象用完就释放
 var a = 123;
 var b1 = new Number(a);
 
 console.log(a == b1);//true,b1隐式转换为基本类型
 console.log(a === b1);//false
+
+var b=false;
+console.log(b.toString());//"false"
 
 //基本类型和引用类型的对象区别
 //基本类型
@@ -15,3 +20,4 @@ var arr = [1,2,3,4，5，6];
 console.log(arr.length);//6
 arr.length = 2;
 console.log(arr.length,arr);//2 [1,2]
+
